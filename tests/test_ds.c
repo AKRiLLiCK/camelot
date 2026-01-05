a2f2597e3627d43c6edee630a9e45fa569ca4790
AKRiLLiCK <akrillick@proton.me>
2026-01-05 15:13:38 +0100

    Attempt 8

--- tests/test_ds.c
+++ tests/test_ds.c
@@ -23,15 +23,24 @@ TEST(test_paged_list) {
   REQUIRE(nums.count == 300);
 
   int *val1 = list.get(&nums, 10);
-  REQUIRE(*val1 == 10);
+  REQUIRE(val1 != NULL);
+  if (val1) {
+    REQUIRE(*val1 == 10);
+  }
 
   int *val2 = list.get(&nums, 260);
-  REQUIRE(*val2 == 260);
+  REQUIRE(val2 != NULL);
+  if (val2) {
+    REQUIRE(*val2 == 260);
+  }
 
   list.remove(&nums, 10);
   int *new_val1 = list.get(&nums, 10);
 
-  REQUIRE(*new_val1 == 299);
+  REQUIRE(new_val1 != NULL);
+  if (new_val1) {
+    REQUIRE(*new_val1 == 299);
+  }
   REQUIRE(nums.count == 299);
 
   arena.release(&a);
@@ -49,7 +58,9 @@ TEST(test_hash_table) {
 
   int *got1 = table.get(&cfg, string.from("Width"));
   REQUIRE(got1 != NULL);
-  REQUIRE(*got1 == 100);
+  if (got1) {
+    REQUIRE(*got1 == 100);
+  }
 
   int *missing = table.get(&cfg, string.from("Depth"));
   REQUIRE(missing == NULL);
